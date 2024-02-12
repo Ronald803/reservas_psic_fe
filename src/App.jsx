4e8ef907f9acc343d53c44bc744485cf0402d32c
Ronald803 <ronaldblancobalboa@gmail.com>
2024-02-11 23:10:05 -0400

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      App de reservas para consultorio psicológico
+    </>
+  )
+}
+
+export default App
